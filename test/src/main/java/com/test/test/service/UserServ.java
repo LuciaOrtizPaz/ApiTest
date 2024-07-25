67636dc0e1d71e5aa344923bf267a69c04808a14
Luciaortizzz <luciaizabelortizpaz556@gmail.com>
2024-07-24 21:30:48 -0600

    SwaggerComit

---
+++ test/src/main/java/com/test/test/service/UserServ.java
@@ -0,0 +1,12 @@
+package com.test.test.service;
+
+import com.api.school.dto.UserDto;
+import java.util.List;
+
+public interface UserServ {
+    List<UserDto> getAll();
+    UserDto getById(long id);
+    UserDto save(UserDto userDto);
+    UserDto update(UserDto userDto, long id);
+    void delete(long id);
+}

6b71dedc584b1cef840f1cea6f9e2195c3776303
Luciaortizzz <luciaizabelortizpaz556@gmail.com>
2024-07-24 21:25:20 -0600

    SwaggenrComit

--- test/src/main/java/com/test/test/TestApplication.java
+++ test/src/main/java/com/test/test/TestApplication.java
@@ -2,8 +2,11 @@ package com.test.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API School", version = "1.0", description = "API for School Management"))
 public class TestApplication {
 
 	public static void main(String[] args) {
